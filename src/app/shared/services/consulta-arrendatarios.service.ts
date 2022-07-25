f93fad81fe6565cf0779fa566329a64f80466684
mBaenaCh <mateo.baena@hotmail.com>
2022-07-24 21:50:06 -0500

    Definicion consumo servicio back

--- src/app/shared/services/consulta-arrendatarios.service.ts
+++ src/app/shared/services/consulta-arrendatarios.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PersonaModel } from '../models/persona';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,10 @@ export class ConsultaArrendatariosService {
 
   urlBase: string;
   constructor(private readonly http: HttpClient) { 
-    
+    this.urlBase = "http://localhost:8080/api/arrendatarios";
+  }
+
+  obtenerArrendatariosHistorial(nombrePropiedad: string): Observable<PersonaModel>{
+    return this.http.get<PersonaModel>(`${this.urlBase}/${nombrePropiedad}`);
   }
 }
